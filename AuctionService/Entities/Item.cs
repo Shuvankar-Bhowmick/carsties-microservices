eb6695a9900c0ccdfd5689437dfa859789017f43
shuvankar-bhowmick <bhowmickshuvankar@gmail.com>
2024-09-17 23:05:21 +0530

    Test pipeline in TeamCity

--- AuctionService/Entities/Item.cs
+++ AuctionService/Entities/Item.cs
@@ -14,6 +14,8 @@ public class Item
     public int Mileage { get; set; }
     public string ImageUrl { get; set; }
 
+    
+    // example comment
     // nav properties for EF Core
     public Auction Auction { get; set; } // navigation property
     public Guid AuctionId { get; set; }  // foreign key property (optional when navigation property is already present)
