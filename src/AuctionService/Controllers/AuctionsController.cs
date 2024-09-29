a57786a8508532e650e9ec37001d5e06deac531c
shuvankar-bhowmick <bhowmickshuvankar@gmail.com>
2024-09-29 22:01:42 +0530

    Add code for CreateAuction endpoint in AuctionsController.cs

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,6 +1,8 @@
 using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,24 +20,48 @@ public class AuctionsController(
     [HttpGet]
     public async Task<ActionResult<List<AuctionResponseDto>>> GetAllAuctions()
     {
-        var auctions = _context.Auctions
+        var auctions = await _context.Auctions
             .Include(x => x.Item)
             .OrderBy(x => x.Item.Make)
             .ToListAsync();
-        
+
         return _mapper.Map<List<AuctionResponseDto>>(auctions);
     }
 
     [HttpGet("{id:guid}", Name = "GetAuction")]
-    public async Task<ActionResult<AuctionResponseDto>> GetAuction(Guid id)
+    public async Task<ActionResult<AuctionResponseDto>> GetAuctionById(
+        Guid id,
+        CancellationToken cancellationToken)
     {
         var auction = await _context.Auctions
             .Include(x => x.Item)
             .FirstOrDefaultAsync(x => x.Id == id);
-        
+
         if (auction == null)
             return NotFound();
-        
+
         return _mapper.Map<AuctionResponseDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionResponseDto>> CreateAuction(
+        [FromBody] CreateAuctionRequestDto createAuctionRequestDto,
+        CancellationToken cancellationToken)
+    {
+        var auction = _mapper.Map<Auction>(createAuctionRequestDto);
+
+        // TODO: add current user as seller
+        auction.Seller = "test";
+
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync(cancellationToken) > 0;
+
+        if (!result)
+            return BadRequest("Could not save changes to the DB");
+
+        // need to tell the user that we created resource and also where it was created at
+        return CreatedAtAction(nameof(GetAuctionById), 
+            new { id = auction.Id }, _mapper.Map<AuctionResponseDto>(auction));
     }
 }
