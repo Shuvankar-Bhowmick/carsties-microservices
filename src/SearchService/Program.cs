a2269fb485630d787495cb9a1a2fc5552a097d06
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2025-01-25 21:12:18 +0530

    Add new microservice named SearchService for searching.

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,14 +1,30 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
 // Add services to the container.
+
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseAuthorization();
 
 app.MapControllers();
+
+// initialize the mongodb database
+await DB.InitAsync("SearchDb",
+    MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDbConnection")));
+
+// create indexes for optimized search
+DB.Index<Item>()
+    .Key(x => x.Make, KeyType.Text)
+    .Key(x => x.Model, KeyType.Text)
+    .Key(x => x.Color, KeyType.Text)
+    .CreateAsync();
 
 app.Run();
