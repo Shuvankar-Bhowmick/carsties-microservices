b8589700b2de93159606b4cd695545d647808ee4
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2025-02-05 21:31:31 +0530

    Add SearchController.cs and Db initializer with seed data for SearchService

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    /* Using ActionResult instead of IActionResult because it
+     gives us type safety */
+    [HttpGet]
+    public async Task<ActionResult<Item>> SearchItems(string searchTerm)
+    {
+        
+    }
+}
