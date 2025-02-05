b83597854d98e4a8bdc95b78d7be8ce8808bf536
Shuvankar Bhowmick <bhowmickshuvankar@gmail.com>
2025-02-05 21:32:09 +0530

    Refactor DbInitializer and seed data for auctions

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Data;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        // initialize the mongodb database
+        await DB.InitAsync("SearchDb",
+            MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        // create indexes for optimized search
+        DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<Item>();
+
+        if (count == 0)
+        {
+            Console.WriteLine("No data - will attempt to seed");
+            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+    }
+}
